1355cce32b081a20003f22266965f41c4e834782
Noncsi <lengyel.noemi96@gmail.com>
2022-11-20 23:37:55 +0100

    implement new UI - create logo and main page

--- quizgame2-ts/src/components/StartGameButtonComponent.tsx
+++ quizgame2-ts/src/components/StartGameButtonComponent.tsx
@@ -2,31 +2,21 @@ import React from "react";
 import { Button } from "@mui/material";
 
 export default function StartGameButtonComponent(props: any) {
-  const startGame = () => {
-    props.setGame({
-      ...props.game,
-      isGameStage: true,
-    });
-  };
-
   return (
     <>
       <Button
         onClick={startGame}
         variant="contained"
         sx={{
-          boxShadow:
-            "0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 82px #f400ff, 0 0 92px #f400ff, 0 0 102px #f400ff",
           fontFamily: "BeautySchoolDropOut",
           fontSize: "3pc;",
-          background: "transparent",
           color: "white",
           borderRadius: "100px",
-          height: "200px",
-          width: "200px",
+          height: "120px",
+          width: "250px",
         }}
       >
-        Start
+        New Game
       </Button>
     </>
   );
